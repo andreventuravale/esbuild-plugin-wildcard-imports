93c234ad97060e5499320f4cf4f643506d3da3b8
Andre Vale <andreventuravale@gmail.com>
2024-02-28 16:12:52 -0300

    tests/esm-import-export-nested/expected.js

--- tests/esm-import-export-nested/expected.js
+++ tests/esm-import-export-nested/expected.js
@@ -1,6 +1,6 @@
 export const message = `
     The "./foo/bar/index.js" path should be an empty object.
-    Since the "^0.0.1" version, nested glob imports are not supposed to work.
+    Since the "0.0.1" version, nested glob imports are not supposed to work.
 `
 
 export default {
