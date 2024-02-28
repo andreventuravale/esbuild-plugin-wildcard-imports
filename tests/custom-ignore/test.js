007c75da6a7848312a6caa8d30688e537c760d08
Andre Vale <andreventuravale@gmail.com>
2024-02-28 20:42:06 -0300

    tests/custom-ignore/test.js

--- tests/custom-ignore/test.js
+++ tests/custom-ignore/test.js
@@ -8,10 +8,12 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const __workdir = join(__dirname, 'fixtures')
 
-test.serial('custom ignore - control', async t => {
+let sequence = 0
+
+test.serial('custom ignore - control ( no ignore )', async t => {
   await testCase()
 
-  const { default: actual } = await import('./dist/stdin.js?_=1')
+  const { default: actual } = await import(`./dist/stdin.js?_=${sequence++}`)
 
   const expected = {
     default: {
@@ -30,11 +32,45 @@ test.serial('custom ignore - control', async t => {
 test.serial('custom ignore - ignores a single path', async t => {
   await testCase(['**/grault*'])
 
-  const { default: actual } = await import('./dist/stdin.js?_=2')
+  const { default: actual } = await import(`./dist/stdin.js?_=${sequence++}`)
 
   const expected = {
     default: {
       './foo/bar/baz.js': { default: 'baz', name: 'baz' },
+      './foo/qux/waldo.js': { default: 'waldo', name: 'waldo' }
+    }
+  }
+
+  t.deepEqual(
+    actual,
+    expected
+  )
+})
+
+test.serial('custom ignore - ignores a many paths using a single pattern', async t => {
+  await testCase(['**/{grault,waldo}*'])
+
+  const { default: actual } = await import(`./dist/stdin.js?_=${sequence++}`)
+
+  const expected = {
+    default: {
+      './foo/bar/baz.js': { default: 'baz', name: 'baz' }
+    }
+  }
+
+  t.deepEqual(
+    actual,
+    expected
+  )
+})
+
+test.serial('custom ignore - ignores a many paths using distinct patterns', async t => {
+  await testCase(['**/baz*', '**/grault*'])
+
+  const { default: actual } = await import(`./dist/stdin.js?_=${sequence++}`)
+
+  const expected = {
+    default: {
       './foo/qux/waldo.js': { default: 'waldo', name: 'waldo' }
     }
   }
